cd3e0e9639497a8c3211278e5678f6c2e8bff809
Petr Sládek <petr.sladek@kelio.cz>
2015-02-13 11:06:32 +0100

    Lockscreen - layout + simply funciton PIN and Call Owner

--- app/src/main/java/cz/vutbr/fit/stud/xslade12/lostphone/LockScreenActivity.java
+++ app/src/main/java/cz/vutbr/fit/stud/xslade12/lostphone/LockScreenActivity.java
@@ -1,9 +1,17 @@
 package cz.vutbr.fit.stud.xslade12.lostphone;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LockScreenActivity extends Activity {
@@ -11,26 +19,80 @@ public class LockScreenActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //Set up our Lockscreen
+        makeFullScreen();
+
         setContentView(R.layout.activity_lock_screen);
     }
 
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.lock_screen, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.lock_screen, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+    /**
+     * A simple method that sets the screen to fullscreen.  It removes the Notifications bar,
+     *   the Actionbar and the virtual keys (if they are on the phone)
+     */
+    public void makeFullScreen() {
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if(Build.VERSION.SDK_INT < 19) { //View.SYSTEM_UI_FLAG_IMMERSIVE is only on API 19+
+            this.getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        } else {
+            this.getWindow().getDecorView()
+                    .setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE);
         }
-        return super.onOptionsItemSelected(item);
     }
+
+//    @Override
+//    public void onBackPressed() {
+//        return; //Do nothing!
+//    }
+//
+//    public void unlockScreen(View view) {
+//        //Instead of using finish(), this totally destroys the process
+//        android.os.Process.killProcess(android.os.Process.myPid());
+//    }
+
+
+
+    public void onClickBtnCallOwner(View view) {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:123456789"));
+        startActivity(callIntent);
+    }
+
+
+
+    public void onClickBtnUnlock(View view) {
+        EditText editUnlock = (EditText) findViewById(R.id.editUnlock);
+        String pin = editUnlock.getText().toString();
+
+        if(pin.equals("1234")) {
+            this.finish();
+        } else {
+            Toast.makeText(this, R.string.wrongPin, Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
